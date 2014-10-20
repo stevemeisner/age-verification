048835f3d823dd147520d057a296caa2ff82f46c
Steve Meisner <steve@websitesthatdostuff.com>
2014-10-20 08:34:59 -0400

    Changed the Demo theme to be very simple and moved the js and css into thier own files.
    Signed-off-by: Steve Meisner <steve@websitesthatdostuff.com>

---
+++ js/age-verification.js
@@ -0,0 +1,96 @@
+/*!
+ * Simple Age Verification (https://github.com/Herudea/age-verification))
+ */
+
+var modal_content,
+modal_screen;
+
+// Start Working ASAP.
+$(document).ready(function() {
+	av_legality_check();
+});
+
+// Make sure the prompt stays in the middle.
+$(window).resize(function() {
+	av_positionPrompt();
+});
+
+av_legality_check = function() {
+	if ($.cookie('is_legal') == "yes") {
+		//legal!
+		// Do nothing?
+	} else {
+		av_showmodal();
+	}
+};
+
+av_showmodal = function() {
+	modal_screen = $('<div id="modal_screen"></div>');
+	modal_content = $('<div id="modal_content" style="display:none"></div>');
+	var modal_content_wrapper = $('<div id="modal_content_wrapper"></div>');
+	var modal_regret_wrapper = $('<div id="modal_regret_wrapper" style="display:none;"></div>');
+
+	// Question Content
+	var content_heading = $('<h2>Are you 21 or older?</h2>');
+	var content_text = $('<p>You must verify that you are 21 years of age or older to enter this site.</p>');
+	var content_buttons = $('<nav><a href="#nothing" class="av_btn av_go" rel="yes">I\'m old enough</a> <a href="#nothing" class="av_btn av_no" rel="no">I\'m NOT old enough</a></nav');
+
+	// Regret Content
+	var regret_heading = $('<h2>We\'re Sorry!</h2>');
+	var regret_text = $('<p>You must be 21 years of age or older to enter this site.</p>');
+	var regret_buttons = $('<nav><small>I hit the wrong button!</small> <a href="#nothing" class="av_btn av_go" rel="yes">I\'m old enough!</a></nav');
+
+	modal_content_wrapper.append(content_heading, content_text, content_buttons);
+	modal_regret_wrapper.append(regret_heading, regret_text, regret_buttons);
+	modal_content.append(modal_content_wrapper, modal_regret_wrapper);
+
+	// Append the prompt to the end of the document
+	$('body').append(modal_screen, modal_content);
+
+	// Center the box
+	av_positionPrompt();
+
+	modal_content.find('a.av_btn').on('click', av_setCookie);
+};
+
+av_setCookie = function(e) {
+	e.preventDefault();
+
+	var is_legal = $(e.currentTarget).attr('rel');
+
+	$.cookie('is_legal', is_legal, {
+		expires: 30,
+		path: '/'
+	});
+
+	if (is_legal == "yes") {
+		av_closeModal();
+	} else {
+		av_showRegret();
+	}
+};
+
+av_closeModal = function() {
+	modal_content.fadeOut();
+	modal_screen.fadeOut();
+};
+
+av_showRegret = function() {
+	modal_screen.css({
+		backgroundColor: 'rgba(0,0,0,.9)'
+	});
+	modal_content.find('#modal_content_wrapper').hide();
+	modal_content.find('#modal_regret_wrapper').show();
+};
+
+av_positionPrompt = function() {
+	var top = ($(window).outerHeight() - $('#modal_content').outerHeight()) / 2;
+	var left = ($(window).outerWidth() - $('#modal_content').outerWidth()) / 2;
+	modal_content.css({
+		'top': top,
+		'left': left
+	});
+
+	if (modal_content.is(':hidden'))
+		modal_content.fadeIn('slow')
+};
